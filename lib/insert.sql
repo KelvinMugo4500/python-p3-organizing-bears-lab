37bc69d8879ac610507be53fe064f29c98d525a2
Kelvin Mugo Maina <kelvinmugo912@gmail.com>
2024-06-08 12:32:20 +0300

    initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 11, "M", "Black", "Playful", "T");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 15, "M", "White", "Joyful", "T");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 8, "F", "Cinnamon", "Fearful", "T");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 14, "M", "Brown", "Empathetic", "T");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 20, "F", "Blue-gray", "Social", "T");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 18, "M", "White", "Altruistic", "T");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 5, "F", "Brown", "Playful", "T");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, "F", "Black", "Joyful", "F");
